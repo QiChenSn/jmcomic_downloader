ef28b66c4b5e3d681cf38976bced9404f5e7f083
FateTrial <87477817+FateTrial@users.noreply.github.com>
2025-02-08 19:00:50 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+from astrbot.api.message_components import *
+from astrbot.api.message_components import File
+from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.star import Context, Star, register
+from astrbot.api.all import *
+
+import httpx
+import json
+import asyncio
+
+import jmcomic
+# 导入此模块，需要先安装（pip install jmcomic -i https://pypi.org/project -U）
+# 创建配置对象
+# 注册插件的装饰器
+@register("JMdownloader", "FateTrial", "一个下载JM本子的插件", "1.0.0")
+class JMPlugin(Star):
+    def __init__(self, context: Context):
+        super().__init__(context)
+        self.cd = 10  # 默认冷却时间为 10 秒
+        self.last_usage = {} # 存储每个用户上次使用指令的时间
+# 注册指令的装饰器。指令名为 JM下载。注册成功后，发送 `/JM下载` 就会触发这个指令
+    @filter.command("JM下载")
+    async def JMid(self, event: AstrMessageEvent):
+        path = os.path.abspath(os.path.dirname(__file__))
+        messages = event.get_messages()
+        if not messages:
+            yield event.plain_result("请输入要下载的本子ID")
+            return
+        # 获取原始消息文本
+        message_text = messages[0].text  
+        # 直接获取 Plain 对象的 text 属性
+        parts = message_text.split()  
+        # 分割消息文本
+        if len(parts) < 2:  
+            # 检查是否有本子ID
+            yield event.plain_result("请输入要下载的本子ID")
+            return
+        tokens = parts[1]  
+        # 获取本子ID
+        option = jmcomic.create_option_by_file(path + "/option.yml")
+        # 使用option对象来下载本子
+        jmcomic.download_album(tokens, option)
+        # 等价写法: option.download_album(422866)
+        yield event.plain_result("已启用下载线程")
+        yield event.chain_result(
+            [File(name=f"{tokens}.pdf", file=f"{path}/pdf/{tokens}.pdf")]
+        )
